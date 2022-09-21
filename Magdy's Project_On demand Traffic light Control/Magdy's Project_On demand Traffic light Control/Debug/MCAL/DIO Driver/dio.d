8ca9429b8ecee4bda515b958f4850cfcc748e025
Magdy Ahmed <91482114+MagdyMGZ@users.noreply.github.com>
2022-09-21 21:04:31 +0200

    Add files via upload

---
+++ Magdy's Project_On demand Traffic light Control/Magdy's Project_On demand Traffic light Control/Debug/MCAL/DIO Driver/dio.d
@@ -0,0 +1,22 @@
+MCAL/DIO Driver/dio.d MCAL/DIO Driver/dio.o: ../MCAL/DIO\ Driver/dio.c \
+ ../MCAL/DIO\ Driver/dio.h \
+ ../MCAL/DIO\ Driver/../../Utilities/registers.h \
+ ../MCAL/DIO\ Driver/../../Utilities/types.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ ../MCAL/DIO\ Driver/../../Utilities/types.h \
+ ../MCAL/DIO\ Driver/../../Utilities/bit_manipulation.h
+
+../MCAL/DIO\ Driver/dio.h:
+
+../MCAL/DIO\ Driver/../../Utilities/registers.h:
+
+../MCAL/DIO\ Driver/../../Utilities/types.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+../MCAL/DIO\ Driver/../../Utilities/types.h:
+
+../MCAL/DIO\ Driver/../../Utilities/bit_manipulation.h:
